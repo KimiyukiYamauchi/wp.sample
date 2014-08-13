5b81f8477e9d3c47f179c3c99fbb373693a2ffec
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-08-13 21:47:46 +0900

    Step2-7まで

--- index.php
+++ index.php
@@ -3,10 +3,40 @@
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>;charset=<?php bloginfo('charset'); ?>" />
 <title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
 </head>
 <body>
 
-<h1><?php bloginfo('name'); ?></h1>
+<!-- container -->
+<div id="container">
+
+<!-- header -->
+<div id="header">
+<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+<p id="desc"><?php bloginfo('description'); ?></p>
+<p id="image"><img src="<?php bloginfo('template_url'); ?>/header.jpg" alt="*" width="760" height="200" /></p>
+</div>
+
+<!-- content -->
+<div id="content">
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+
+<div class="post">
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+<?php the_content(); ?>
+
+<p class="postinfo">
+<?php echo get_the_date(); ?> <?php the_time(); ?>
+ | 
+	 category : <?php the_category(', '); ?>
+</p>
+</div>
+
+<?php endwhile; endif; ?>
+</div>
+
+</div>
 	
 </body>
 </html>
